8cb3712d43e8f4318b06030e036814070ee312c4
Hassan Ahmed Hassan <hasanhazem2016@gmail.com>
2026-09-25 20:21:14 +0300

    Add Day 103 LeetCode 977 Squares of a Sorted Array solution

--- problems/day-103/Squares_of_a_Sorted_Array.cpp
+++ problems/day-103/Squares_of_a_Sorted_Array.cpp
@@ -31,3 +31,43 @@ Date:
 2026-09-24
 ==================================================
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> result(n);
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while(left <= right)
+        {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if(leftSquare > rightSquare)
+            {
+                result[index] = leftSquare;
+                left++;
+            }
+            else
+            {
+                result[index] = rightSquare;
+                right--;
+            }
+
+            index--;
+        }
+
+        return result;
+    }
+};
